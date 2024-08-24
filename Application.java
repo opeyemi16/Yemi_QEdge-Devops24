625b211e8a43b12d5e3b2d36b9e53ad27a2b009e
opeyemi16 <opeyemi16@gmail.com>
2024-08-24 18:16:36 -0400

    feature201 changes in Application.java file by Yemi

--- Application.java
+++ Application.java
@@ -16,7 +16,22 @@ public class DemoClass{
                }
           }
  
-  
+  #feature201 changes by Developer Yemi for the module paymentgateway oct2023
+ public class Contains {
+    public static void main(String[] args) {
+        String str1 = "The Topic is: Java String contains";
+        String str2 = "Java";
+        if(str1.contains(str2)) {
+            System.out.println("Returns True");
+        }
+        else {
+            System.out.println("Returns False");
+        }
+         
+         
+    }
+}end of feature201 code
+ 
  public static void main (String[] args){
     System.out.println("Hello , this is sample program");   
     String grade = DemoClass.getGrade(70);
