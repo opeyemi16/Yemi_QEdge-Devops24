6be10d81ae3e28ec6c41c9ab186a4837f2e7f9ec
opeyemi16 <opeyemi16@gmail.com>
2024-08-24 18:44:27 -0400

    feature202 changes by Leye

--- Application.java
+++ Application.java
@@ -15,8 +15,22 @@ public class DemoClass{
                    return "Not Eligible"; //Return statement
                }
           }
+ #feature202 changes on reports module by Leye in oct2023
+  public class CompareTo {
  
-  
+    public static void main(String[] args) {
+         
+        String str1 = "Software Testing";
+        String str2 = "Software Testing Help";
+         
+        System.out.println(str1.compareTo(str2));
+        // Since str2 contains 5 characters more than the str1, output should be -5
+         
+        System.out.println(str2.compareTo(str1));
+        // Since str2 contains 5 characters less than the str1, output should be +5
+    }
+}
+#end of feature202 changes
  public static void main (String[] args){
     System.out.println("Hello , this is sample program");   
     String grade = DemoClass.getGrade(70);
